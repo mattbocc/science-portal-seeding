b3b995ca1dea9f4bd4c1b004ba05b1cf84056ca2
mattbocc <matthew.boccalon@live.ca>
2023-10-25 17:00:08 -0400

    Adjustments to how citations are scraped, images are downloaded, and database is seeded

--- citations.py
+++ citations.py
@@ -1,30 +1,54 @@
 import os
 from dotenv import load_dotenv
 from scholarly import scholarly
+import certifi
 import pymongo
+import random
+import time
+import json
+
+# Load .env
+load_dotenv()
 
 # MongoDB connection parameters
-client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+client = pymongo.MongoClient(os.getenv("MONGO_URI"), tlsCAFile=certifi.where())
 db_name = client[os.getenv("DB_NAME")]
 collection = db_name[os.getenv("COLLECTION_NAME")]
+
+no_scholar_list = []
 
 # Iterate through each document in the collection
 for document in collection.find():
     name = document.get('name', '')
+    doi = document.get('doi', '')
     
-    # Search for the publication on Google Scholar
-    search_publication = scholarly.search_single_pub(name)
-    
-    if search_publication:
-        # Get the number of citations
-        num_citations = search_publication.get('num_citations', 0)
+    try:
+        # Search for the publication on Google Scholar by doi
+        search_publication = scholarly.search_single_pub(doi)
         
-        # Update the 'citations' field in the document
-        collection.update_one(
-            {"_id": document["_id"]},
-            {"$set": {"citations": num_citations}}
-        )
-    else:
-        print(f"Publication '{name}' not found on Google Scholar.")
+        if search_publication is not None:
+            # Get the number of citations
+            num_citations = int(search_publication.get('num_citations', 0))
+
+            if num_citations != 0:
+                # Update the 'citations' field in the document
+                collection.update_one(
+                    {"_id": document["_id"]},
+                    {"$set": {"citations": num_citations}}
+                )
+                print(f'{name} entered {num_citations}')
+            else:
+                print(f"No citations found for publication '{name}'.")
+        else:
+            print(f"Publication '{name}' not found on Google Scholar.")
+    except:
+        no_scholar_list.append(name)
+        print(f'Cannot get citations for {name} publication')
+    
+    time.sleep(random.randint(8, 20))
 
 client.close()
+
+# Write no_scholar_list to a JSON file
+with open('no_scholar_list.json', 'w') as json_file:
+    json.dump(no_scholar_list, json_file)
