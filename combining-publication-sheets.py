a58f60cc625a50d4b488c30f191fb850ef4a5332
mattbocc <matthew.boccalon@live.ca>
2024-05-22 14:02:16 -0400

    Created a script to combine all Research Analytics sheets. Added external scraping for journal, publisher, and date. Added special cases for it journal DNE, added special cases if publisher DNE, added special cases if both journal and publisher don't exist, added doi validity check, extract unique journals, extract doi fails, utilized crossref for reliable and easy scraping

--- combining-publication-sheets.py
+++ combining-publication-sheets.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 # Load the Excel files
 df_2022 = pd.read_excel('./input-data/publication-lists/UHN-publications-2022.xlsm', sheet_name="2022", header=0)
@@ -12,5 +13,41 @@ print(df_2024.columns)
 # Combine the DataFrames
 df_combined = pd.concat([df_2022, df_2023, df_2024], ignore_index=True)
 
-# Optionally, save the combined DataFrame to a new csv file
-df_combined.to_csv('./input-data/publication-lists/UHN-publications-combined.csv', index=False)
+# Chose which columns are wanted and rename accordingly
+selected_columns = ['PMID', 'doi', 'Date', 'Title', 'Journal', 'Article Type', 'Author List', 'Filtered Author List', 'All Affiliations']
+df_selected = df_combined[selected_columns]
+df_selected = df_selected.rename(columns={
+    'Date': 'date',
+    'Title': 'name',
+    'Journal': 'journal',
+    'Article Type': 'type',
+	'Author List': 'authors',
+    'Filtered Author List': 'filteredAuthors',
+    'All Affiliations': 'affiliations',
+})
+
+df_selected['PMID'] = pd.to_numeric(df_selected['PMID'], errors='coerce').astype(pd.Int64Dtype())
+
+#Filter dataframe based on type and affiliation criteria
+filtered_selected_df = df_selected[
+    (
+        (df_selected['type'].str.contains('research-article', case=False, na=False))
+        | (df_selected['type'].str.contains('Article', case=False, na=False))
+    )
+    & (~df_selected['type'].str.contains('early-review', case=False, na=False))
+    & (~df_selected['type'].str.contains('Early Access', case=False, na=False))
+    & (~df_selected['type'].str.contains('brief-report', case=False, na=False))
+    & (~df_selected['type'].str.contains('Review', case=False, na=False))
+    & (~df_selected['type'].str.contains('Video-Audio Media', case=False, na=False))
+    & 
+    (
+        (df_selected['affiliations'].str.contains('Princess Margaret', case=False, na=False))
+        | (df_selected['affiliations'].str.contains('PMC', case=False, na=False))
+    )
+]
+
+filtered_selected_df['affiliations'] = filtered_selected_df['affiliations'].str.replace('/', '')
+
+
+# Save the combined DataFrame to a new csv file
+filtered_selected_df.to_csv('./input-data/publication-lists/UHN-publications-combined.csv', index=False)
---
+++ crossref-scrape.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import requests
+import time
+import json
+import datetime
+
+doi_fails = []
+no_journal_or_publisher = []
+
+def fetch_crossref_data(doi, email):
+    """Fetch publication data from CrossRef API using the DOI."""
+    url = f"https://api.crossref.org/works/{doi}"
+    headers = {
+        'User-Agent': f'Python-requests/ {requests.__version__}',
+        'Mailto': email
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        created_date = data['message']['created']['date-time'][:10]  # Extract only the date part YYYY-MM-DD
+
+        #If journal title exists retrieve it, else create list of dois with no journals
+        journal_title = data['message'].get('container-title')
+
+        #If journal title exists retrieve it, else create list of dois with no journals
+        publisher = data['message']['publisher']
+
+        if journal_title:
+            journal_title = journal_title[0]
+        else:
+            if publisher == '':
+                no_journal_or_publisher.append(doi)
+                print(f"No journal or publisher {doi}")
+            journal_title = ''
+        return created_date, journal_title, publisher
+    except requests.RequestException as e:
+        print(f"Error fetching data for DOI {doi}: {str(e)}")
+        doi_fails.append(doi)
+        print(doi_fails)
+        return None, None, None
+
+def update_dataframe(csv_path, output_path, email):
+    """Update the DataFrame with new Date and Journal information from CrossRef."""
+    df = pd.read_csv(csv_path)
+
+    # Add initialize new columns and set their default values
+    df['citations'] = 0
+    df['rating'] = ""
+    df['status'] = "Published"
+    df['dateAdded'] = str(datetime.datetime.now())[:10]
+
+    # Iterate over each row in the DataFrame
+    for index, row in df.iterrows():
+        if pd.notna(row['doi']):  # Check if DOI exists
+            created_date, journal_title, publisher = fetch_crossref_data(row['doi'], email)
+            if created_date and journal_title and publisher:
+                df.at[index, 'date'] = created_date
+                df.at[index, 'journal'] = journal_title
+                df.at[index, 'publisher'] = publisher
+        time.sleep(0.25)
+        print(f"Updated row {index + 1}/{len(df)}")
+
+    # # Convert journal names to titles (capitalize first letter of each 
+    # df['journal'] = df['journal'].str.title()
+
+    # Extract all unique journals into a JSON file
+    unique_journals = df['journal'].unique().tolist()
+    unique_journal_total = len(unique_journals)
+    print(f"Total unique journals {unique_journal_total}")
+
+    with open("./output-data/json-logs/unique_journals.json", 'w') as f:
+        json.dump(unique_journals, f)
+
+    with open("./output-data/json-logs/doi_fails.json", 'w') as f:
+        json.dump(doi_fails, f)
+
+    with open("./output-data/json-logs/no_journal.json", 'w') as f:
+        json.dump(no_journal_or_publisher, f)
+
+    # Extract journals, set to lowercase, replace spaces with underscores and add to the image slot
+    lowercase_journal = df['journal'].str.lower()
+    df['image'] = lowercase_journal.str.replace(' ', '_') + '.jpg'
+
+    df.to_csv(output_path, index=False)
+    print("Update complete and CSV saved.")
+
+your_email = 'matthew.boccalon@uhn.ca'
+input_csv_path = './input-data/publication-lists/UHN-publications-combined.csv'
+output_csv_path = './output-data/UHN-publications-crossref-update.csv'
+
+update_dataframe(input_csv_path, output_csv_path, your_email)
