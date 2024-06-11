d9239d38347a34fc35424ef07598e3629e836949
mattbocc <matthew.boccalon@live.ca>
2024-06-11 10:46:18 -0400

    Fix for author extraction

--- author-extraction.py
+++ author-extraction.py
@@ -8,8 +8,8 @@ load_dotenv()
 input_file = os.getenv("AUTHOR_SHEET")
 data = pd.read_csv(f'./input-data/{input_file}', encoding='ISO-8859-1')
     
-filtered_data = data[data['Primary Research Institute'] == 'PM']
-filtered_data = data[data['Primary Appointment'].str.contains('scientist', case=False)]
+filtered_data = data[data['primaryResearchInstitute'] == 'PM']
+filtered_data = filtered_data[filtered_data['primaryAppointment'].str.contains('scientist', case=False)]
     
 filtered_data.to_csv(f'./output-data/authors.csv', index=False)
 
